730a7c94dc9e505abcd1dffe77dc8810c8831091
John Hawthorn <john.hawthorn@gmail.com>
2017-10-12 10:38:52 -0700

    SONIC REDUCER!!!!

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@
 task :console do
   $LOAD_PATH.unshift 'lib'
   require 'sudoku'
+  require 'reducer'
   require 'pry'
   Pry.start
 end
--- lib/reducer.rb
+++ lib/reducer.rb
@@ -3,7 +3,16 @@ class Reducer
     @sudoku = sudoku
   end
 
-  def reduce
-    @sudoku
+  def reduce_once
+    best = @sudoku
+    (0...9).each do |row|
+      (0...9).each do |col|
+        current = best.remove(col, row)
+        if current.possiblities_for(col, row).count == 1
+          best = current
+        end
+      end
+    end
+    best
   end
 end
