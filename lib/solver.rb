dee0f7dea217a141d65f8b7102d99021b0a85f3e
John Hawthorn <john.hawthorn@gmail.com>
2017-10-12 11:25:23 -0700

    Add solver

---
+++ lib/solver.rb
@@ -0,0 +1,29 @@
+class Solver
+  def initialize(sudoku)
+    @initial = sudoku
+  end
+
+  def solve
+    best = @initial
+    loop do
+      current = fill_in_known(best)
+      if current.count > best.count
+        best = current
+      else
+        break
+      end
+    end
+    best
+  end
+
+  def fill_in_known(sudoku)
+    sudoku.each_missing do |col, row|
+      possibilities = sudoku.possiblities_for(col, row)
+
+      if possibilities.count == 1
+        sudoku = sudoku.set(col, row, possibilities[0])
+      end
+    end
+    sudoku
+  end
+end
