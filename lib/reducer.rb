8088dc1aea05be0bf06b4de66b3e4c6569653eee
John Hawthorn <john.hawthorn@gmail.com>
2017-10-12 12:37:10 -0700

    Add brutereducer

--- lib/reducer.rb
+++ lib/reducer.rb
@@ -73,6 +73,19 @@ class ColumnReducer < SimpleReducer
   end
 end
 
+class BruteReducer < BaseReducer
+  def reduce
+    best = @sudoku
+    best.each_filled do |col, row|
+      attempt = best.remove(col, row)
+      if attempt.solve.solved?
+        best = attempt
+      end
+    end
+    best
+  end
+end
+
 class Reducer < BaseReducer
   RANDOM_PASSES = ENV['ITERATIONS'] ? ENV['ITERATIONS'].to_i : 1000
 
@@ -84,6 +97,8 @@ class Reducer < BaseReducer
         best = solution
       end
     end
+
+    best = BruteReducer.new(best).reduce
 
     best
   end
